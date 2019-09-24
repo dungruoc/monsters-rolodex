a7d940fde78497317c4cc10c985f02ef8ef55682
dungruoc <minhdung.dang@gmail.com>
2019-09-24 14:01:00 +0700

    Updates

---
+++ precache-manifest.7939caec2acec25ca6cc93b9bc156a10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e15fc55621d97be33fda778b690ec3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c1be3a367ad5ecadae8d",
+    "url": "/monsters-rolodex/static/css/main.3a5ae961.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "c1be3a367ad5ecadae8d",
+    "url": "/monsters-rolodex/static/js/main.50827dd6.chunk.js"
+  },
+  {
+    "revision": "29b8639263678424eef0",
+    "url": "/monsters-rolodex/static/js/runtime-main.7352cc78.js"
+  }
+]);
